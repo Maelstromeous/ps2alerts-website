f9b764a6f2c638644ce1a1c366b6541da9fd535c
Maelstrome26 <maelstrome26@gmail.com>
2015-12-29 01:11:28 +0000

    Removed obsolete env requirements

--- config/config.php
+++ config/config.php
@@ -4,11 +4,5 @@ return [
     'environment' => $_ENV['ENV'],
     'base_url'    => $_ENV['BASE_URL'],
     'asset_url'   => $_ENV['BASE_URL'] . '/assets',
-    'api_url'     => $_ENV['API_URL'],
-    'database'    => [
-        'host'     => $_ENV['DB_HOST'],
-        'user'     => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASS'],
-        'schema'   => $_ENV['DB_NAME']
-    ]
+    'api_url'     => $_ENV['API_URL']
 ];
