40cd487e24089cacaeabe25e7c8b3298590c60de
Maelstrome26 <maelstrome26@gmail.com>
2015-12-29 17:54:09 +0000

    Added domination calls

--- public/assets/js/homepage/alert.totals.js
+++ public/assets/js/homepage/alert.totals.js
@@ -36,12 +36,18 @@ function getEmpireVictories() {
         readAlertTotals({ ResultWinner: 'nc' }),
         readAlertTotals({ ResultWinner: 'tr' }),
         readAlertTotals({ ResultWinner: 'draw' }),
+        readAlertTotals({ ResultWinner: 'vs', ResultDomination: 1 }),
+        readAlertTotals({ ResultWinner: 'nc', ResultDomination: 1 }),
+        readAlertTotals({ ResultWinner: 'tr', ResultDomination: 1 }),
     ]).then(function(totals) {
         // Write to the statistics object
         alertStats.victories.vs   = totals[0];
         alertStats.victories.nc   = totals[1];
         alertStats.victories.tr   = totals[2];
         alertStats.victories.draw = totals[3];
+        alertStats.dominations.vs = totals[4];
+        alertStats.dominations.nc = totals[5];
+        alertStats.dominations.tr = totals[6];
 
         writeEmpireTotals();
     }).catch(function(error) {
@@ -50,14 +56,24 @@ function getEmpireVictories() {
 }
 
 function writeTotals() {
-    
+
 }
 
 function writeEmpireTotals() {
     var empires = ['vs', 'nc', 'tr', 'draw'];
 
     for (var i = 0; i < empires.length; i++) {
-        $('#' + empires[i] + '-victories').text(alertStats.victories[empires[i]]);
+        var elem = $('#' + empires[i] + '-victories');
+        var percentage = alertStats.victories[empires[i]] / alertStats.total * 100;
+
+        $(elem).html('<b>' + alertStats.victories[empires[i]] + '</b> / ' + percentage.toFixed(2) + '%');
+    }
+
+    // Deletes Draw for domination loop
+    delete empires[3];
+
+    for (var i = 0; i < empires.length; i++) {
+        $('#' + empires[i] + '-dominations').text(alertStats.dominations[empires[i]]);
     }
 
     $('.victory-card .card .fa-spin').hide();
