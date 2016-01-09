bc102cdd88e9c896f004d2fcb048fcb01fab5041
Matthew Cavanagh <maelstrome26@gmail.com>
2016-01-09 17:53:49 +0000

    ...

--- public/assets/js/common/global.js
+++ public/assets/js/common/global.js
@@ -1,25 +1,3 @@
-var vsColor   = '#61088F';
-var ncColor   = '#2732A8';
-var trColor   = '#A90000';
-var drawColor = '#4B4B4B';
-
-var servers  = [1,10,13,17,25,1000,2000];
-var zones    = [2,4,6,8];
-var factions = ['vs','nc','tr','draw'];
-
-var debug = 1;
-
-var serverNames = {
-    1: 'Connery',
-    10: 'Miller',
-    13: 'Cobalt',
-    17: 'Emerald',
-    19: 'Jaeger',
-    25: 'Briggs',
-    1000: 'Genudine',
-    2000: 'Ceres'
-};
-
 // Global function which takes a territory bar element and data, and then renders
 // the bar with unform widths etc.
 function renderTerritoryBar(data, elem, numbers, showOpposite) {
