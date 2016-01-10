5d299a48002f2044c8fd6e0dbbc2feb4831eea23
Matthew Cavanagh <maelstrome26@gmail.com>
2016-01-10 17:41:35 +0000

    Removed locationProvider. Confusing me.

--- public/app.js
+++ public/app.js
@@ -1,29 +1,23 @@
 /* use strict */
 
 var app = angular.module('Ps2Alerts', ['ngRoute'])
-.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
-        .when('/home', {
-            template: 'Hello home!'
+        .when('/', {
+            templateUrl: "partials/home/index.html"
         })
         .when('/pageTwo', {
             templateUrl: "partials/home/page2.html",
         })
         .otherwise({
-            redirectTo: "/home"
+            redirectTo: "/"
         });
-
-    $locationProvider.html5Mode({
-        enabled: true,
-        requireBase: false
-    });
 }]);
 
 app.controller('MainController', function($scope) {
     $scope.labelName = "New Button";
     $scope.newElement = angular.element('<div class="btn btn-waves">' +
         $scope.labelName + '</div>');
-
 });
 
 app.controller('MainPageCtrl', function($scope) {
