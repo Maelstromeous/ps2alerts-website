5dc4826bde322a588c425068f2575ff3390eafaa
Maelstrome26 <maelstrome26@gmail.com>
2016-01-11 17:52:34 +0000

    Updated various assets

---
+++ public/directives/home.js
@@ -0,0 +1,11 @@
+app.directive('announcement', function() {
+    return {
+        templateUrl: 'views/home/announcement.html'
+    };
+});
+
+app.directive('homeTabs', function() {
+    return {
+        templateUrl: 'views/home/tabs.html'
+    };
+});
