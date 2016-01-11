1713dc384d2a23f85a0c7b126e18bf33f5b5f79c
Maelstrome26 <maelstrome26@gmail.com>
2016-01-11 01:48:40 +0000

    Got it working :)

--- public/controllers/home.js
+++ public/controllers/home.js
@@ -1,9 +1,5 @@
 app.controller('VictoryController', function($scope, ENV, AlertStatisticsService) {
     $scope.today = new Date();
 
-    $scope.incrementTotal = function(by) {
-        AlertStatisticsService.incrementTotal(by);
-    };
-
-    $scope.alertTotal = AlertStatisticsService.total;
+    $scope.AlertStats = AlertStatisticsService;
 });
