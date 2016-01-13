bccfb5fba9619773b59ac7be4ba202926f78afb2
Maelstrome26 <maelstrome26@gmail.com>
2016-01-13 22:43:16 +0000

    Added more to the config array

--- public/services/configDataService.js
+++ public/services/configDataService.js
@@ -5,6 +5,16 @@ app.service('ConfigDataService', function(ENV) {
     factory.apiUrl = ENV.apiUrl;
 
     factory.factions = ['vs','nc','tr','draw'];
+    factory.servers = [1,10,13,17,25,1000,2000];
+    factory.serverNames = {
+        1:    'Connery',
+        10:   'Miller',
+        13:   'Cobalt',
+        17:   'Emerald',
+        25:   'Briggs',
+        1000: 'Genudine',
+        2000: 'Ceres'
+    };
 
     return factory;
 });
