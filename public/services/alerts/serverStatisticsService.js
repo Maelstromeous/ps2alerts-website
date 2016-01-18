907234820953a65a523ebf4389d645119f7b1b17
Maelstrome26 <maelstrome26@gmail.com>
2016-01-18 01:16:53 +0000

    Cleanup

--- public/services/alerts/serverStatisticsService.js
+++ public/services/alerts/serverStatisticsService.js
@@ -1,15 +1,3 @@
 app.service('ServerStatisticsService', function ($http, $log, ConfigDataService) {
-    var factory = {
-        victories: {}
-    };
-
-    angular.forEach(ConfigDataService.servers, function(server) {
-        factory.victories[server] = {};
-        angular.forEach(ConfigDataService.factions, function(faction) {
-            factory.victories[server][faction] = 0;
-        });
-    });
-
-    $log.log(factory);
-    return factory;
+    
 });
