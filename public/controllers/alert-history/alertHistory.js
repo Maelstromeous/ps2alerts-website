ab342399e3feb8b79490e38a50a586e8259da993
Matthew Cavanagh <maelstrome26@gmail.com>
2016-02-14 23:07:53 +0000

    Added input elements for alert history

--- public/controllers/alert-history/alertHistory.js
+++ public/controllers/alert-history/alertHistory.js
@@ -1,4 +1,12 @@
 app.controller('AlertHistoryController', function($scope, ConfigDataService, AlertHistoryService) {
     $scope.data = AlertHistoryService;
     $scope.config = ConfigDataService;
+
+    setTimeout(function() {
+        $('select').material_select();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+    })
 });
