1ce090bddf4295866f9e8dc762684ceeb15a8c02
Matthew Cavanagh <maelstrome26@gmail.com>
2016-02-15 02:05:57 +0000

    Sorted out alert history inputs. Decided to convert to using buttons rather than selects.

--- public/controllers/alert-history/alertHistory.js
+++ public/controllers/alert-history/alertHistory.js
@@ -1,12 +1,67 @@
-app.controller('AlertHistoryController', function($scope, ConfigDataService, AlertHistoryService) {
+app.controller('AlertHistoryController', function($scope, $log, ConfigDataService, AlertHistoryService) {
     $scope.data = AlertHistoryService;
     $scope.config = ConfigDataService;
 
-    setTimeout(function() {
-        $('select').material_select();
-        $('.datepicker').pickadate({
-            selectMonths: true, // Creates a dropdown to control month
-            selectYears: 15 // Creates a dropdown of 15 years to control year
-        });
-    })
+    $scope.selectedServers  = [1,10,13,17,25,1000,2000]; // Set Default
+    $scope.selectedZones    = [2,4,6,8];
+    $scope.selectedBrackets = ['MOR','AFT','PRI'];
+    $scope.selectedFactions = ['VS','NC','TR','DRAW'];
+
+    $scope.toggleServer = function(server) {
+        var index = $scope.selectedServers.indexOf(server);
+        if (index > -1) {
+            $scope.selectedServers.splice(index, 1); // Remove element from array
+        } else {
+            $scope.selectedServers.push(server);
+        }
+    };
+
+    $scope.toggleZone = function(zone) {
+        var index = $scope.selectedZones.indexOf(zone);
+        if (index > -1) {
+            $scope.selectedZones.splice(index, 1); // Remove element from array
+        } else {
+            $scope.selectedZones.push(zone);
+        }
+    };
+
+    $scope.toggleBracket = function(bracket) {
+        var index = $scope.selectedBrackets.indexOf(bracket);
+        if (index > -1) {
+            $scope.selectedBrackets.splice(index, 1); // Remove element from array
+        } else {
+            $scope.selectedBrackets.push(bracket);
+        }
+    }
+
+    $scope.toggleFaction = function(faction) {
+        var index = $scope.selectedFactions.indexOf(faction);
+        if (index > -1) {
+            $scope.selectedFactions.splice(index, 1); // Remove element from array
+        } else {
+            $scope.selectedFactions.push(faction);
+        }
+    }
+
+    $scope.filter = function() {
+        $log.log($scope.selectedServers);
+    }
+
+    $scope.started = new Date('2014-10-30');
+    $scope.today   = new Date();
+
+    $scope.minDate = new Date(
+        $scope.started.getFullYear(),
+        $scope.started.getMonth(),
+        $scope.started.getDate()
+    );
+
+    $scope.maxDate = new Date(
+        $scope.started.getFullYear(),
+        $scope.started.getMonth(),
+        $scope.started.getDate()
+    );
+
+    $scope.dateFrom = new Date();
+    $scope.dateTo   = new Date();
 });
--- public/services/configDataService.js
+++ public/services/configDataService.js
@@ -5,7 +5,15 @@ app.service('ConfigDataService', function(ENV) {
     factory.apiUrl = ENV.apiUrl;
 
     factory.factions = ['vs','nc','tr','draw'];
+    factory.factionsAlpha = {
+        'vs': 'Vanu Sovereignty',
+        'nc': 'New Conglomerate',
+        'tr': 'Terran Republic',
+        'draw': 'Draw'
+    };
+    factory.factionsNumeric = [1,2,3,-1];
     factory.servers = [1,10,13,17,25,1000,2000];
+    factory.serversAlpha = [25,13,1,17,10,1000,2000];
     factory.serverNames = {
         1:    'Connery',
         10:   'Miller',
@@ -18,11 +26,17 @@ app.service('ConfigDataService', function(ENV) {
         2001: 'Palos'
     };
     factory.zones = [2,4,6,8];
+    factory.zonesAlpha = [6,8,4,2];
     factory.zoneNames = {
         2: 'Indar',
         4: 'Hossin',
         6: 'Amerish',
         8: 'Esamir'
+    };
+    factory.timeBrackets = {
+        'MOR': 'Morning (00:00 - 11:59)',
+        'AFT': 'Afternoon (12:00 - 16:59)',
+        'PRI': 'Prime TIme (17:00 - 23:59)'
     };
 
     factory.convertFactionNameToInt = function(name) {
