b360630d4937a7f9f1bf613902138012d9e25b5d
Maelstrome26 <maelstrome26@gmail.com>
2016-02-24 21:56:59 +0000

    Ditto

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,9 @@ module.exports = function(grunt) {
                 files: {
                     'public/assets/js/deps.js': [
                         'public/bower_components/jquery/dist/jquery.min.js',
-                        'public/bower_components/datatables/media/js/jquery.dataTables.min.js',
                         'public/bower_components/angular/angular.min.js',
                         'public/bower_components/angular-route/angular-route.min.js',
-                        'public/bower_components/angular-datatables/dist/angular-datatables.min.js',
+                        'public/bower_components/angular-smart-table/dist/smart-table.js',
                         'public/bower_components/jquery.countdown/dist/jquery.countdown.min.js',
                         'public/assets/js/jsloader.js',
                         'public/bower_components/lodash/dist/lodash.min.js',
@@ -48,8 +47,7 @@ module.exports = function(grunt) {
                 files: {
                     'public/assets/css/main.css': [
                         'public/bower_components/SpinKit/css/spinners/7-three-bounce.css',
-                        'public/assets/css/compiled/main.css',
-                        'public/bower_components/datatables/media/css/jquery.dataTables.min.css'
+                        'public/assets/css/compiled/main.css'
                     ]
                 }
             }
