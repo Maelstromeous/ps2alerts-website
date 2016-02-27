0c41e71388280800f5077f2ca7a94b5b24b803e9
Matt Cavanagh <maelstrome26@gmail.com>
2016-02-27 07:32:54 +0000

    Added back homepage announcement

--- public/controllers/home/home.js
+++ public/controllers/home/home.js
@@ -1,7 +1,22 @@
-app.controller('HomeVictoryController', function($scope, ConfigDataService, HomeStatisticsService) {
+app.controller('HomeVictoryController', function($scope, $rootScope, ConfigDataService, HomeStatisticsService) {
     $scope.today = new Date();
     $scope.alertStats = HomeStatisticsService;
     $scope.config = ConfigDataService;
+
+    $('body').on('click', "#announcement-view-more", function() {
+        var expanded = $("#announcement-view-more").attr('expanded');
+        console.log(expanded);
+
+        if (expanded == "0") {
+            $("#announcement-more").slideDown();
+            $("#announcement-last").removeClass('row-no-margin');
+            $("#announcement-view-more").html('Hide').attr('expanded', 1);
+        } else {
+            $("#announcement-more").slideUp();
+            $("#announcement-last").addClass('row-no-margin');
+            $("#announcement-view-more").html('Read more...').attr('expanded', 0);
+        }
+    });
 });
 
 app.controller('HomeTimelineController', function ($scope) {
