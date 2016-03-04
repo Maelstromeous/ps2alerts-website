4737d69cc6a31aa45e414759d5355386d68c7d68
Matt Cavanagh <maelstrome26@gmail.com>
2016-03-04 18:32:24 +0000

    Added ability to resize the project status partial

--- public/directives/common/project.status.js
+++ public/directives/common/project.status.js
@@ -4,6 +4,7 @@ app.directive('projectStatus', function() {
         scope : {
             cardTitle:    '@',
             cardSubtitle: '@',
+            sizing:       '@',
             project:      '='
         },
         templateUrl: 'views/common/partials/project.status.html',
