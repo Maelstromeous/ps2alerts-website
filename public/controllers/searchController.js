d5de9fc8a7e1e23cb7bf91dff5ec5bda1568aa10
Matt Cavanagh <maelstrome26@gmail.com>
2016-03-11 19:21:00 +0000

    Formating

--- public/controllers/searchController.js
+++ public/controllers/searchController.js
@@ -2,7 +2,7 @@ app.controller('SearchController', function($scope, SearchService) {
     $scope.type        = 'player';
     $scope.term        = '';
     $scope.placeholder = '';
-    $scope.service = SearchService
+    $scope.service = SearchService;
 
     $scope.fireSearch = function() {
         console.log('Firing search');
@@ -10,15 +10,15 @@ app.controller('SearchController', function($scope, SearchService) {
         if ($scope.term.length > 1) {
             SearchService.search($scope.type, $scope.term);
         }
-    }
+    };
 
     $scope.hide = function() {
         $scope.service.results = [];
         $scope.term = '';
         $scope.service.show = false;
-    }
+    };
 
     $scope.$watch('type', function() {
         $scope.placeholder = 'Search ' + _.capitalize($scope.type) + 's';
-    })
+    });
 });
