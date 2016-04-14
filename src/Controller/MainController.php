06b05b1f3b1c80cd6db1e375a47b417eb4e53e1c
Matthew Cavanagh <maelstrome26@gmail.com>
2016-04-15 00:07:15 +0100

    Attempt to diagnose ConfigAware issues

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,8 @@
 
 namespace Ps2alerts\Frontend\Controller;
 
+use Ps2alerts\Frontend\Contract\ConfigAwareInterface;
+use Ps2alerts\Frontend\Contract\ConfigAwareTrait;
 use Ps2alerts\Frontend\Contract\EmitterAwareInterface;
 use Ps2alerts\Frontend\Contract\EmitterAwareTrait;
 use Ps2alerts\Frontend\Contract\TemplateAwareInterface;
@@ -9,13 +11,17 @@ use Ps2alerts\Frontend\Contract\TemplateAwareTrait;
 use Zend\Diactoros\ServerRequest;
 use Zend\Diactoros\Response;
 
-class MainController implements EmitterAwareInterface, TemplateAwareInterface
+class MainController implements ConfigAwareInterface, EmitterAwareInterface, TemplateAwareInterface
 {
+    use ConfigAwareTrait;
     use EmitterAwareTrait;
     use TemplateAwareTrait;
 
     public function landing(ServerRequest $request)
     {
+        var_dump('CONFIG SHOULD WORK HERE');
+        var_dump($this);
+        var_dump($this->getConfig());die;
         return $this->getEmitterDriver()->emit('render.template', [
             'title' => 'Test',
             'code'  => 200
