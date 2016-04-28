3aa31d54e6dac4f54b440879e4aeb23f4b6516fe
Matt Cavanagh <maelstrome26@gmail.com>
2016-04-28 22:08:14 +0100

    Completed majority of Outfit Profiles page

--- public/controllers/profiles/outfitProfileController.js
+++ public/controllers/profiles/outfitProfileController.js
@@ -25,8 +25,8 @@ app.controller('OutfitProfileController', function(
                 { data: 'id', title: 'ID', className: 'id center-align' },
                 { data: 'kills', title: 'Kills', className: 'metric center-align' },
                 { data: 'deaths', title: 'Deaths', className: 'metric center-align' },
-                { data: 'teamkills' , title: 'TeamKills', className: 'metric center-align' },
-                { data: 'suicides' , title: 'Suicides', className: 'metric center-align' }
+                { data: 'teamkills', title: 'TeamKills', className: 'metric center-align' },
+                { data: 'suicides', title: 'Suicides', className: 'metric center-align' }
             ],
             order:          [0, 'desc'],
             deferRender:    true,
@@ -36,6 +36,28 @@ app.controller('OutfitProfileController', function(
             searching:      false,
             "rowCallback": function( row, data, index ) {
                 $('.id', row).html('<a href="alert/' + data.id + '">' + data.id + '</a>');
+            }
+        });
+
+        $('#player-list').DataTable({
+            data: $scope.service.data.players.data,
+            columns: [
+                { data: 'name', title: 'Player', className: 'player' },
+                { data: 'kills', title: 'Kills', className: 'metric center-align' },
+                { data: 'deaths', title: 'Deaths', className: 'metric center-align' },
+                { data: 'teamkills', title: 'TeamKills', className: 'metric center-align' },
+                { data: 'suicides', title: 'Suicides', className: 'metric center-align' },
+                { data: 'headshots', title: 'Headshots', className: 'metric center-align' },
+                { data: 'id', visible: false }
+            ],
+            order:          [1, 'desc'],
+            deferRender:    true,
+            scrollY:        450,
+            scrollCollapse: true,
+            scroller:       true,
+            searching:      true,
+            "rowCallback": function( row, data, index ) {
+                $('.player', row).html('<a href="profiles/player/' + data.id + '">' + data.name + '</a>');
             }
         });
     });
