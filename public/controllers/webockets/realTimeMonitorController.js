798d79bb5f8b20a4459772a5a4f401b6a4da8390
Matthew Cavanagh <maelstrome26@gmail.com>
2016-04-29 18:26:41 +0100

    Refined setting of messages and other CSS tweaks

--- public/controllers/webockets/realTimeMonitorController.js
+++ public/controllers/webockets/realTimeMonitorController.js
@@ -4,6 +4,7 @@ app.controller('RealTimeMonitorController', function($scope, ConfigDataService, 
 
     $scope.websocketDisabled = 1;
     $scope.message = 'DBG API is reporting incorrect territory values. Tracking has been disabled until this is resolved.';
+    $scope.messageCaption = 'Updated: 29th April 15:51 UTC';
 
     if ($scope.websocketDisabled === 0) {
         // Start the engines
