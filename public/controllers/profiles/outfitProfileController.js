249d9d98fe110bd6fb6fbc65abe6be2f658c8889
Matt Cavanagh <maelstrome26@gmail.com>
2016-04-29 15:55:46 +0100

    Added checks to make sure that the data being parsed was present

--- public/controllers/profiles/outfitProfileController.js
+++ public/controllers/profiles/outfitProfileController.js
@@ -19,64 +19,70 @@ app.controller('OutfitProfileController', function(
 
         $scope.$emit('ga-sync', '#outfit-profile .ga-event');
 
-        $('#player-list').DataTable({
-            data: $scope.service.data.players.data,
-            columns: [
-                { data: 'name', title: 'Player', className: 'player' },
-                { data: 'kills', title: 'Kills', className: 'metric center-align' },
-                { data: 'deaths', title: 'Deaths', className: 'metric center-align' },
-                { data: 'teamkills', title: 'TKs', className: 'metric center-align' },
-                { data: 'suicides', title: 'Suicides', className: 'metric center-align' },
-                { data: 'headshots', title: 'Headshots', className: 'metric center-align' },
-                { data: 'id', visible: false }
-            ],
-            order:          [1, 'desc'],
-            deferRender:    true,
-            scrollY:        450,
-            scrollCollapse: true,
-            scroller:       true,
-            searching:      true,
-            "rowCallback": function( row, data, index ) {
-                $('.player', row).html('<a href="profiles/player/' + data.id + '">' + data.name + '</a>');
-            }
-        });
+        if ($scope.service.data.players.data.length > 0) {
+            $('#player-list').DataTable({
+                data: $scope.service.data.players.data,
+                columns: [
+                    { data: 'name', title: 'Player', className: 'player' },
+                    { data: 'kills', title: 'Kills', className: 'metric center-align' },
+                    { data: 'deaths', title: 'Deaths', className: 'metric center-align' },
+                    { data: 'teamkills', title: 'TKs', className: 'metric center-align' },
+                    { data: 'suicides', title: 'Suicides', className: 'metric center-align' },
+                    { data: 'headshots', title: 'Headshots', className: 'metric center-align' },
+                    { data: 'id', visible: false }
+                ],
+                order:          [1, 'desc'],
+                deferRender:    true,
+                scrollY:        450,
+                scrollCollapse: true,
+                scroller:       true,
+                searching:      true,
+                "rowCallback": function( row, data, index ) {
+                    $('.player', row).html('<a href="profiles/player/' + data.id + '">' + data.name + '</a>');
+                }
+            });
+        }
 
-        $('#facility-list').DataTable({
-            data: $scope.service.data.facilities.data,
-            columns: [
-                { data: 'name', title: 'Facility', className: 'name' },
-                { data: 'captures', title: 'Captures', className: 'metric center-align' },
-                { data: 'defences', title: 'Defences', className: 'metric center-align' },
-                { data: 'zone', title: 'Continent', className: 'center-align' },
-                { data: 'type', title: 'Type', className: 'center-align' }
-            ],
-            order:          [1, 'desc'],
-            deferRender:    true,
-            scrollY:        450,
-            scrollCollapse: true,
-            scroller:       true,
-            searching:      true
-        });
+        if ($scope.service.data.facilities.data.length > 0) {
+            $('#facility-list').DataTable({
+                data: $scope.service.data.facilities.data,
+                columns: [
+                    { data: 'name', title: 'Facility', className: 'name' },
+                    { data: 'captures', title: 'Captures', className: 'metric center-align' },
+                    { data: 'defences', title: 'Defences', className: 'metric center-align' },
+                    { data: 'zone', title: 'Continent', className: 'center-align' },
+                    { data: 'type', title: 'Type', className: 'center-align' }
+                ],
+                order:          [1, 'desc'],
+                deferRender:    true,
+                scrollY:        450,
+                scrollCollapse: true,
+                scroller:       true,
+                searching:      true
+            });
+        }
 
-        $('#alert-list').DataTable({
-            data: $scope.service.data.involvement.data,
-            columns: [
-                { data: 'id', title: 'ID', className: 'id center-align' },
-                { data: 'kills', title: 'Kills', className: 'metric center-align' },
-                { data: 'deaths', title: 'Deaths', className: 'metric center-align' },
-                { data: 'teamkills', title: 'TKs', className: 'metric center-align' },
-                { data: 'suicides', title: 'Suicides', className: 'metric center-align' }
-            ],
-            order:          [0, 'desc'],
-            deferRender:    true,
-            scrollY:        354,
-            scrollCollapse: true,
-            scroller:       true,
-            searching:      false,
-            "rowCallback": function( row, data, index ) {
-                $('.id', row).html('<a href="alert/' + data.id + '">' + data.id + '</a>');
-            }
-        });
+        if ($scope.service.data.involvement.data.length > 0) {
+            $('#alert-list').DataTable({
+                data: $scope.service.data.involvement.data,
+                columns: [
+                    { data: 'id', title: 'ID', className: 'id center-align' },
+                    { data: 'kills', title: 'Kills', className: 'metric center-align' },
+                    { data: 'deaths', title: 'Deaths', className: 'metric center-align' },
+                    { data: 'teamkills', title: 'TKs', className: 'metric center-align' },
+                    { data: 'suicides', title: 'Suicides', className: 'metric center-align' }
+                ],
+                order:          [0, 'desc'],
+                deferRender:    true,
+                scrollY:        354,
+                scrollCollapse: true,
+                scroller:       true,
+                searching:      false,
+                "rowCallback": function( row, data, index ) {
+                    $('.id', row).html('<a href="alert/' + data.id + '">' + data.id + '</a>');
+                }
+            });
+        }
 
         setTimeout(function() {
             $('.tooltipped').tooltip({
