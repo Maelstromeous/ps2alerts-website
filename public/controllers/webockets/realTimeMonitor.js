c6ada6264bf6734308380afded37b3fa2abe5a6a
Matthew Cavanagh <maelstrome26@gmail.com>
2016-04-29 18:04:52 +0100

    Added ability to wilfully disable monitor and add a message

--- public/controllers/webockets/realTimeMonitor.js
+++
@@ -1,7 +0,0 @@
-app.controller('RealTimeMonitorController', function($scope, ConfigDataService, WebsocketService, $interval) {
-    $scope.config    = ConfigDataService;
-    $scope.websocket = WebsocketService;
-
-    // Start the engines
-    $scope.websocket.initWebSocket();
-});
