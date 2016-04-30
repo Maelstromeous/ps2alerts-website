8b8339efeccc86ffce1116a27ef5fad08eee3c68
Matthew Cavanagh <maelstrome26@gmail.com>
2016-04-30 14:24:31 +0100

    Tweaked monitor some more. Reenabled monitor.

--- public/controllers/webockets/realTimeMonitorController.js
+++ public/controllers/webockets/realTimeMonitorController.js
@@ -2,9 +2,9 @@ app.controller('RealTimeMonitorController', function($scope, ConfigDataService, 
     $scope.config    = ConfigDataService;
     $scope.websocket = WebsocketService;
 
-    $scope.websocketDisabled = 1;
-    $scope.message = 'DBG API is reporting incorrect territory values. Tracking has been disabled until this is resolved.';
-    $scope.messageCaption = 'Updated: 29th April 15:51 UTC';
+    $scope.websocketDisabled = 0;
+    $scope.message = 'DBG API is occasionally reporting incorrect territory values. Tracking may be inaccurate for some servers and continents (which appears to be seemingly random). Please report them as you see them. The results will be removed accordingly upon manual detection.';
+    $scope.messageCaption = 'Updated: 30th April 15:51 UTC';
 
     if ($scope.websocketDisabled === 0) {
         // Start the engines
