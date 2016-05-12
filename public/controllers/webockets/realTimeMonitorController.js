5b314b9a2a1f14d26481ace940efda6055702e60
Matthew Cavanagh <maelstrome26@gmail.com>
2016-05-12 21:20:06 +0100

    Re-enabled monitor

--- public/controllers/webockets/realTimeMonitorController.js
+++ public/controllers/webockets/realTimeMonitorController.js
@@ -2,9 +2,9 @@ app.controller('RealTimeMonitorController', function($scope, ConfigDataService, 
     $scope.config    = ConfigDataService;
     $scope.websocket = WebsocketService;
 
-    $scope.websocketDisabled = 1;
-    $scope.message = 'DBG API is reporting incorrect territory values. Alert tracking has been disabled until this has been fixed.';
-    $scope.messageCaption = 'Updated: 29th April 13:51 UTC';
+    $scope.websocketDisabled = 0;
+    $scope.message = '';
+    $scope.messageCaption = '';
 
     if ($scope.websocketDisabled === 0) {
         // Start the engines
