ae32012df7626bd91f226c647a26bddaaf890fb6
Matthew Cavanagh <maelstrome26@gmail.com>
2016-06-12 00:27:54 +0100

    Added Faction Victory Contribution statistics

--- public/controllers/home/homeZoneVictoryController.js
+++ public/controllers/home/homeZoneVictoryController.js
@@ -1,13 +1,12 @@
-app.controller('HomeZoneVictoryController', function($scope, HomeZoneVictoryService) {
+app.controller('HomeZoneVictoryController', function($scope, HomeZoneVictoryService, ConfigDataService) {
     $scope.zoneStats = HomeZoneVictoryService;
+    $scope.config = ConfigDataService;
     $scope.zoneStats.init();
+    $scope.loaded = false;
 
     $scope.$on('zonesLoaded', function() {
+        $scope.loaded = true;
         $scope.$apply(); // Fix for slow loading
         console.log($scope.zoneStats);
-    })
-
-    $scope.calculateServerEmpireStats = function() {
-
-    }
+    });
 });
--- public/services/home/homeZoneVictoryService.js
+++ public/services/home/homeZoneVictoryService.js
@@ -2,7 +2,10 @@ app.service('HomeZoneVictoryService', function ($http, $rootScope, ConfigDataSer
     var factory = {
         zones: {},
         zoneTotals: {},
-        serverEmpireStats: {}
+        serverEmpireStats: {},
+        serverEmpireTotals: {
+            all: 0
+        }
     };
 
     factory.init = function() {
@@ -21,6 +24,13 @@ app.service('HomeZoneVictoryService', function ($http, $rootScope, ConfigDataSer
             });
         });
 
+        _.forEach(ConfigDataService.factions, function (faction) {
+            factory.serverEmpireStats[faction] = {};
+            _.forEach(ConfigDataService.servers, function (server) {
+                factory.serverEmpireStats[faction][server] = 0;
+            });
+        });
+
         Promise.all([
             factory.getZoneData(2),
             factory.getZoneData(4),
@@ -31,9 +41,55 @@ app.service('HomeZoneVictoryService', function ($http, $rootScope, ConfigDataSer
                 factory.handleZoneData(2, result[0]),
                 factory.handleZoneData(4, result[1]),
                 factory.handleZoneData(6, result[2]),
-                factory.handleZoneData(8, result[3]),
-                factory.calculateServerEmpireStats(result)
+                factory.handleZoneData(8, result[3])
             ]).then(function(result) {
+                // Generate Server Contribution Totals
+                _.forEach(factory.serverEmpireStats, function (servers, faction) {
+                    var count = 0;
+                    _.forEach(servers, function(value, server) {
+                        count += value;
+                    });
+                    factory.serverEmpireTotals[faction] = count;
+                    factory.serverEmpireTotals.all += count;
+                });
+
+                // Sort the object by number of wins
+                var sortable = {};
+                _.forEach(factory.serverEmpireStats, function (servers, faction) {
+                    sortable[faction] = [];
+                    _.forEach(servers, function (value, key) {
+                        console.log(value);
+                        sortable[faction].push([key, value]);
+                    });
+                });
+
+                _.forEach(sortable, function (value, faction) {
+                    sortable[faction].sort(function(a, b) {
+                        if (a[1] > b[1]) {
+                            return -1;
+                        }
+
+                        if (a[1] == b[1]) {
+                            return 0;
+                        }
+
+                        return 1;
+                    })
+                });
+
+                // Now we're sorted, rebuild the object
+                factory.serverEmpireStats = {};
+                _.forEach(sortable, function (servers, faction) {
+                    factory.serverEmpireStats[faction] = {};
+
+                    _.forEach(servers, function (values, key) {
+                        factory.serverEmpireStats[faction][key] = {
+                            'server': values[0],
+                            'value': values[1]
+                        };
+                    });
+                });
+                
                 $rootScope.$broadcast('zonesLoaded', 'loaded');
             });
         });
@@ -56,22 +112,14 @@ app.service('HomeZoneVictoryService', function ($http, $rootScope, ConfigDataSer
                 angular.forEach(ConfigDataService.factions, function(faction) {
                     factory.zones[zone][server][faction] = values.data[faction];
                     factory.zoneTotals[zone][faction] += values.data[faction];
+                    factory.serverEmpireStats[faction][server] += values.data[faction];
                 });
                 factory.zones[zone][server].total = values.data.total;
-
             });
 
             return resolve();
         });
     };
-
-    factory.calculateServerEmpireStats = function(data) {
-        return new Promise(function(resolve, reject) {
-            
-
-            return resolve();
-        });
-    }
 
     return factory;
 });
