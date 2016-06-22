41be3cea1d62e8f15af0005a690016736023a8a6
Matthew Cavanagh <maelstrome26@gmail.com>
2016-06-22 23:43:36 +0100

    May help if I uploaded the gruntfile...

--- Gruntfile.js
+++ Gruntfile.js
@@ -102,7 +102,9 @@ module.exports = function(grunt) {
                     ENV: {
                         environment: 'development',
                         baseUrl: 'http://192.168.33.10/ps2alerts/public',
-                        apiUrl: 'http://192.168.33.10/ps2alerts-api/public/v2'
+                        apiUrl: 'http://192.168.33.10/ps2alerts-api/public/v2',
+                        websocketUrl: '192.168.0.3:1337?apikey=692e01b167f4c5c28cdc95389f038393'
+                        //websocketUrl: 'ws.ps2alerts.com:1337?apikey=692e01b167f4c5c28cdc95389f038393'
                     }
                 }
             },
@@ -114,7 +116,8 @@ module.exports = function(grunt) {
                     ENV: {
                         environment: 'staging',
                         baseUrl: 'http://staging.ps2alerts.com',
-                        apiUrl: 'http://staging.api.ps2alerts.com/v2'
+                        apiUrl: 'http://staging.api.ps2alerts.com/v2',
+                        websocketUrl: 'ws.ps2alerts.com:1337?apikey=692e01b167f4c5c28cdc95389f038393'
                     }
                 }
             },
@@ -126,7 +129,8 @@ module.exports = function(grunt) {
                     ENV: {
                         environment: 'production',
                         baseUrl: 'http://www.ps2alerts.com',
-                        apiUrl: 'http://api.ps2alerts.com/v2'
+                        apiUrl: 'http://api.ps2alerts.com/v2',
+                        websocketUrl: 'ws.ps2alerts.com:1337?apikey=692e01b167f4c5c28cdc95389f038393'
                     }
                 }
             },
