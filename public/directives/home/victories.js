d0ec11647f916ba8c63edffe2ca91953e32c9662
Matt Cavanagh <maelstrome26@gmail.com>
2016-08-07 15:06:35 +0100

    Merged all homepage victory directives. Moved homepage victories into their own directive template. Fixed loading anim on metric card.

---
+++ public/directives/home/victories.js
@@ -0,0 +1,45 @@
+app.directive('homeVictories', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'views/home/victories.html'
+    }
+});
+
+app.directive('homeVictoryBreakdowns', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'views/home/victory.breakdowns.html',
+        link: function( $scope, elem, attrs ) {
+            elem.ready(function(){
+                // $scope.$emit('ga-sync', '#combat-leaderboards .ga-event');
+            });
+        }
+    };
+});
+
+app.directive('homeFactionCard', function(HomeStatisticsService) {
+    return {
+        restrict: 'A',
+        scope : {
+            cardClass: '@',
+            cardTitle: '@',
+            faction:   '@',
+        },
+        link:function(scope) {
+            scope.stats = HomeStatisticsService.totals;
+        },
+        templateUrl: 'views/home/partials/faction.card.html',
+    };
+});
+
+app.directive('homeVictoryTimeline', function() {
+    return {
+        restrict: 'A',
+        templateUrl: 'views/home/victory.timeline.html',
+        link: function( $scope, elem, attrs ) {
+            elem.ready(function(){
+                // $scope.$emit('ga-sync', '#combat-leaderboards .ga-event');
+            });
+        }
+    };
+});
